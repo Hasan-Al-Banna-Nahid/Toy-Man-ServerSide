74c533b0090f476246f2e4d03dd877aa10177cc3
Hasan-Al-Banna-Nahid <iamnahid591998@gmail.com>
2023-05-19 23:53:33 +0600

    Created Post API & Send Data to MongoDB

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Toy Man Server Is Running");
 });
-console.log(process.env.DB_UserName, process.env.DB_Pass);
+
 const uri = `mongodb+srv://${process.env.DB_UserName}:${process.env.DB_Pass}@cluster0.pmxdri5.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,6 +26,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    app.post("/toysData", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const toyData = client.db("ToysData").collection("ToyData");
+      const result = await toyData.insertOne(data);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
